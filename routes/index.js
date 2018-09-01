084ccd6a7ed3a2992d51f73afdb4e8a4d0923ff6
Alexandre Barreto de Souza <alexandrebdesouza1@gmail.com>
2018-09-01 11:18:54 -0300

    app

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
+const mongoose = require('mongoose');
+const Produtos = require('./../models/produtos');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({ title: 'Express' });
+router.get('/', function (req, res, next) {
+    Produtos.find({}, function (err,produto) {
+        if(err){
+            console.log(err,'erro get')
+        }
+        res.status(200).send(produto)
+    })
+    res.json({title: 'Express'});
 });
 
 module.exports = router;
+
